f1167b51e809dc06df2e8a2ec90e947b67675fe2
Rafael Herrero <rafahsolis@gmail.com>
2015-03-05 21:30:23 +0100

    Plot 3

--- plot3.R
+++ plot3.R
@@ -25,7 +25,7 @@ MyPlot<-function(datos){
     legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=c(1,1,1))
 }
 
-datos <- load_data("household_power_consumption.txt")
+datos <- load_data("household_power_consumption1.txt")
 set_plot()
 MyPlot(datos)
 dev.off()
