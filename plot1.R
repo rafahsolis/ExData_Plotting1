dc2fe70534c7e1b50d807b0a56fe3ef36018bbe9
Rafael Herrero <rafahsolis@gmail.com>
2015-03-05 19:02:06 +0100

    Plot 1

---
+++ plot1.R
@@ -0,0 +1,27 @@
+set_plot <- function(){
+    png(
+        "plot1.png",
+        width     = 480,
+        height    = 480,
+        units     = "px"
+    )
+}
+
+load_data <-function(filename){
+    my_data <-read.table(filename,header=TRUE,na.strings="?",sep=";")
+    
+    # 2007-02-01 and 2007-02-02
+    filtro <- my_data$Date == "1/2/2007" | my_data$Date == "2/2/2007"
+    my_data <- my_data[filtro,]
+    my_data
+}
+
+MyPlot<-function(datos){
+    hist(datos$Global_active_power, xlab="Global Active Power(kilowatts)", col="red", main="Global Active Power")
+    
+}
+
+datos <- load_data("household_power_consumption1.txt")
+set_plot()
+MyPlot(datos)
+dev.off()
